ba880d298f2b87be57ee64cb8a3fdaaf120269ea
radviger@gmail.com <4804422z>
2019-10-02 23:19:24 +0200

    Probably fixed weirdo crash during forge update.

---
+++ src/main/java/ru/craftlogic/mixin/client/renderer/entity/MixinRenderHusk.java
@@ -0,0 +1,27 @@
+package ru.craftlogic.mixin.client.renderer.entity;
+
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderBiped;
+import net.minecraft.client.renderer.entity.RenderHusk;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.RenderZombie;
+import net.minecraft.entity.monster.EntityHusk;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import ru.craftlogic.api.entity.Zombie;
+
+@Mixin(RenderHusk.class)
+public abstract class MixinRenderHusk extends RenderBiped<EntityHusk> {
+    public MixinRenderHusk(RenderManager renderManager, ModelBiped model, float shadowSize) {
+        super(renderManager, model, shadowSize);
+    }
+
+    @Override
+    protected void preRenderCallback(EntityHusk husk, float p_preRenderCallback_2_) {
+        float size = husk.getRenderSizeModifier();
+        GlStateManager.scale(1.0625F * size, 1.0625F * size, 1.0625F * size);
+    }
+}
