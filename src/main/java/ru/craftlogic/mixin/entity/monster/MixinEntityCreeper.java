392fa0aaf7bffaafc4ce938f0f4280517810e7e4
radviger@gmail.com <4804422z>
2019-10-03 17:08:35 +0200

    Fixed depressed creepers explode.

--- src/main/java/ru/craftlogic/mixin/entity/monster/MixinEntityCreeper.java
+++ src/main/java/ru/craftlogic/mixin/entity/monster/MixinEntityCreeper.java
@@ -8,9 +8,14 @@ import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemShears;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,18 +25,36 @@ import ru.craftlogic.api.entity.Creeper;
 
 @Mixin(EntityCreeper.class)
 public abstract class MixinEntityCreeper extends EntityMob implements Creeper {
+    @Shadow @Final private static DataParameter<Boolean> IGNITED;
+    private static final DataParameter<Boolean> DEPRESSED = EntityDataManager.createKey(EntityCreeper.class, DataSerializers.BOOLEAN);
+
     @Shadow
     public abstract void setCreeperState(int state);
 
     @Shadow public abstract int getCreeperState();
 
+    @Shadow public abstract boolean hasIgnited();
+
+    @Shadow protected abstract void explode();
+
+    @Shadow private int lastActiveTime;
+
+    @Shadow private int timeSinceIgnited;
+
+    @Shadow private int fuseTime;
+
     public MixinEntityCreeper(World world) {
         super(world);
     }
 
+    @Inject(method = "entityInit", at = @At("RETURN"))
+    protected void onInit(CallbackInfo info) {
+        this.dataManager.register(DEPRESSED, false);
+    }
+
     @Override
     public boolean isDepressed() {
-        return this.getCreeperState() == 0;
+        return this.dataManager.get(DEPRESSED);
     }
 
     @Override
@@ -40,7 +63,9 @@ public abstract class MixinEntityCreeper extends EntityMob implements Creeper {
             ItemStack heldItem = player.getHeldItem(hand);
             if (heldItem.getItem() instanceof ItemShears) {
                 heldItem.damageItem(1, player);
-                this.setCreeperState(0);
+                this.setCreeperState(-1);
+                this.dataManager.set(IGNITED, false);
+                this.dataManager.set(DEPRESSED, true);
                 this.tasks.taskEntries.removeIf(e -> e.action instanceof EntityAICreeperSwell);
                 this.playSound(SoundEvents.ENTITY_CREEPER_HURT, 1F, 0.5F);
                 this.dropItem(CraftItems.CREEPER_OYSTERS, 1);
@@ -50,10 +75,48 @@ public abstract class MixinEntityCreeper extends EntityMob implements Creeper {
         return false;
     }
 
+    /**
+     * @author Radviger
+     * @reason Depressed creepers
+     */
+    @Overwrite
+    public void onUpdate() {
+        if (isEntityAlive()) {
+            lastActiveTime = timeSinceIgnited;
+
+            if (isDepressed()) {
+                setCreeperState(-1);
+            } else if (hasIgnited()) {
+                setCreeperState(1);
+            }
+
+            int i = getCreeperState();
+
+            if (i > 0 && timeSinceIgnited == 0) {
+                playSound(SoundEvents.ENTITY_CREEPER_PRIMED, 1F, 0.5F);
+            }
+
+            timeSinceIgnited += i;
+
+            if (timeSinceIgnited < 0) {
+                timeSinceIgnited = 0;
+            }
+
+            if (timeSinceIgnited >= fuseTime) {
+                timeSinceIgnited = fuseTime;
+                explode();
+            }
+        }
+
+        super.onUpdate();
+    }
+
     @Inject(method = "readEntityFromNBT", at = @At("RETURN"))
     public void onNbtRead(NBTTagCompound compound, CallbackInfo info) {
         if (compound.getBoolean("depressed")) {
-            this.setCreeperState(0);
+            this.setCreeperState(-1);
+            this.dataManager.set(IGNITED, false);
+            this.dataManager.set(DEPRESSED, true);
         }
     }
 
