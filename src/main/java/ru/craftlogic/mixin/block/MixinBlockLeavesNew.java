1e6b4b6f35247507f3072bb4a2b7cb518368c1f9
radviger@gmail.com <4804422z>
2019-10-05 23:48:13 +0200

    Sticks drop from leaves.

--- src/main/java/ru/craftlogic/mixin/block/MixinBlockLeavesNew.java
+++ src/main/java/ru/craftlogic/mixin/block/MixinBlockLeavesNew.java
@@ -3,9 +3,13 @@ package ru.craftlogic.mixin.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockNewLeaf;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -13,6 +17,17 @@ import org.spongepowered.asm.mixin.Overwrite;
 public class MixinBlockLeavesNew extends Block {
     public MixinBlockLeavesNew(Material material) {
         super(material);
+    }
+
+    /**
+     * @author Radviger
+     * @reason No apple drops from oak. Drop sticks instead
+     */
+    @Overwrite
+    protected void dropApple(World world, BlockPos pos, IBlockState state, int chance) {
+        if (world.rand.nextFloat() < 0.1F) {
+            spawnAsEntity(world, pos, new ItemStack(Items.STICK));
+        }
     }
 
     /**
--- src/main/java/ru/craftlogic/mixin/block/MixinBlockLeavesOld.java
+++ src/main/java/ru/craftlogic/mixin/block/MixinBlockLeavesOld.java
@@ -3,10 +3,14 @@ package ru.craftlogic.mixin.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockOldLeaf;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -14,6 +18,17 @@ import org.spongepowered.asm.mixin.Overwrite;
 public class MixinBlockLeavesOld extends Block {
     public MixinBlockLeavesOld(Material material) {
         super(material);
+    }
+
+    /**
+     * @author Radviger
+     * @reason No apple drops from oak. Drop sticks instead
+     */
+    @Overwrite
+    protected void dropApple(World world, BlockPos pos, IBlockState state, int chance) {
+        if (world.rand.nextFloat() < 0.1F) {
+            spawnAsEntity(world, pos, new ItemStack(Items.STICK));
+        }
     }
 
     /**
