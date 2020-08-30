203abbaccca62dc0143b3a671f2cba293bc5d035
Radviger <radviger@gmail.com>
2020-08-31 00:59:08 +0200

    Fixed stick drop.

--- src/main/java/ru/craftlogic/common/block/BlockStick.java
+++ src/main/java/ru/craftlogic/common/block/BlockStick.java
@@ -1,16 +1,25 @@
 package ru.craftlogic.common.block;
 
 import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import ru.craftlogic.api.block.BlockLyingItem;
 import ru.craftlogic.api.world.Location;
+
+import java.util.Random;
 
 public class BlockStick extends BlockLyingItem {
     public BlockStick() {
         super("stick", 0F, CreativeTabs.DECORATIONS);
         setSoundType(SoundType.WOOD);
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return Items.STICK;
     }
 
     @Override
