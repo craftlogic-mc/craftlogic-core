933c69db510c964bf6f6df5be8fb3799872e1451
Radviger <radviger@gmail.com>
2021-07-29 18:41:27 +0200

    Fixed mixin redirect in BlockFire.tryCatchFire.

--- src/main/java/ru/craftlogic/mixin/block/MixinBlockFire.java
+++ src/main/java/ru/craftlogic/mixin/block/MixinBlockFire.java
@@ -5,18 +5,20 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Random;
 
 @Mixin(BlockFire.class)
-public class MixinBlockFire {
-    @Inject(method = "tryCatchFire", at = @At("HEAD"), cancellable = true)
-    private void tryCatchFire(World world, BlockPos pos, int chance, Random random, int age, EnumFacing face, CallbackInfo ci) {
-        if (!world.getGameRules().getBoolean("doFireSpread")) {
-            ci.cancel();
+public abstract class MixinBlockFire {
+    @Shadow protected abstract void tryCatchFire(World world, BlockPos pos, int chance, Random random, int age, EnumFacing face);
+
+    @Redirect(method = "updateTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockFire;tryCatchFire(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;ILjava/util/Random;ILnet/minecraft/util/EnumFacing;)V"))
+    private void onCatchFire(BlockFire bf, World world, BlockPos pos, int chance, Random random, int age, EnumFacing face) {
+        if (world.getGameRules().getBoolean("doFireSpread")) {
+            tryCatchFire(world, pos, chance, random, age, face);
         }
     }
 }
