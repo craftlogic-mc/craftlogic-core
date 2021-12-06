c9eb9259e27ade47b63b29def564b5514de3aafc
Radviger <radviger@gmail.com>
2021-12-06 17:48:17 +0200

    Now tab-completing outside of command context spectator player names requires permission too.

--- src/main/java/ru/craftlogic/mixin/server/MixinMinecraftServer.java
+++ src/main/java/ru/craftlogic/mixin/server/MixinMinecraftServer.java
@@ -1,9 +1,15 @@
 package ru.craftlogic.mixin.server;
 
+import net.minecraft.command.ICommandManager;
+import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.PlayerList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,11 +18,17 @@ import ru.craftlogic.CraftConfig;
 import ru.craftlogic.api.CraftAPI;
 import ru.craftlogic.api.server.AdvancedServer;
 import ru.craftlogic.api.server.Server;
+import ru.craftlogic.api.world.CommandSender;
 import ru.craftlogic.network.message.MessageServerCrash;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(MinecraftServer.class)
 public abstract class MixinMinecraftServer implements AdvancedServer {
     @Shadow private PlayerList playerList;
+    @Shadow @Final public ICommandManager commandManager;
     private final Server wrapper = new Server((MinecraftServer)(Object)this);
 
     @Override
@@ -35,5 +47,43 @@ public abstract class MixinMinecraftServer implements AdvancedServer {
         for (EntityPlayerMP player : this.playerList.getPlayers()) {
             CraftAPI.NETWORK.sendTo(player, new MessageServerCrash(delay));
         }
+    }
+
+    /**
+     * @author Radviger
+     * @reason Do not tab-complete spectator players without permission
+     */
+    @Overwrite
+    public List<String> getTabCompletions(ICommandSender sender, String input, @Nullable BlockPos pos, boolean hasTargetBlock) {
+        List<String> result = new ArrayList<>();
+        boolean isCommand = input.startsWith("/");
+        if (isCommand) {
+            input = input.substring(1);
+        }
+
+        if (!isCommand && !hasTargetBlock) {
+            String[] args = input.split(" ", -1);
+            String firstArg = args[args.length - 1];
+            List<EntityPlayerMP> players = this.playerList.getPlayers();
+            CommandSender s = CommandSender.from(wrapper, sender);
+            for (EntityPlayerMP player : players) {
+                if (player.interactionManager.getGameType() != GameType.SPECTATOR || s.hasPermission("command.completion.spectators")) {
+                    result.add(player.getName());
+                }
+            }
+        } else {
+            boolean hasNoSpaces = !input.contains(" ");
+            List<String> variants = this.commandManager.getTabCompletions(sender, input, pos);
+            if (!variants.isEmpty()) {
+                for (String variant : variants) {
+                    if (hasNoSpaces && !hasTargetBlock) {
+                        result.add("/" + variant);
+                    } else {
+                        result.add(variant);
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
