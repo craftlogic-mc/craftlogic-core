2c02fd57d8f5111cdc1271b32621691635572c3f
vkazbanov <vkazbanov@telematika.com>
2024-01-20 13:37:16 +0200

    Fix MixinGuiLanguage$List

--- src/main/java/ru/craftlogic/mixin/client/gui/MixinGuiLanguage$List.java
+++ src/main/java/ru/craftlogic/mixin/client/gui/MixinGuiLanguage$List.java
@@ -1,14 +1,17 @@
 package ru.craftlogic.mixin.client.gui;
 
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.resource.IResourceType;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(targets = "net/minecraft/client/gui/GuiLanguage$List")
 public class MixinGuiLanguage$List {
-    @Redirect(method = "elementClicked(IZII)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;refreshResources()V"))
-    public void refreshResources(Minecraft mc) {
+    @Redirect(method = "elementClicked", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/client/FMLClientHandler;refreshResources ([Lnet/minecraftforge/client/resource/IResourceType;)V", remap = false))
+    public void refreshResources(FMLClientHandler self, IResourceType[] res) {
+        Minecraft mc = self.getClient();
         mc.getLanguageManager().onResourceManagerReload(mc.getResourceManager());
     }
 }
